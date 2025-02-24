5c929848b8c26f2a02b92cd59f46e964199bec0d
yash gupta <yashgtech00@gmail.com>
2025-02-24 18:02:13 +0530

    add

---
+++ server/app.js
@@ -0,0 +1,33 @@
+import express from "express";
+import { Server } from "socket.io";
+import {createServer} from "http";
+
+
+const port = 3000;
+
+const app = express();
+
+const server = createServer(app);
+const io = new Server(server,{
+    cors:{
+        origin:"http://localhost:5173",
+        methods:["GET","POST"],
+        credentials:true
+    }
+});
+
+app.get('/',(req,res)=>{
+    res.send("hello")
+})
+
+io.on("connection",(socket)=>{
+    console.log("user connected");
+    console.log("id",socket.id);
+    socket.emit("welcome",`welcome to sockets ${socket.id}`)
+})
+
+
+server.listen(port,()=>{
+    console.log(`server is listen to port ${port}`);
+    
+})
